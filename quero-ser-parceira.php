3893d953722a551bdfe1b792114c75b8c162d24a
thiagoHagge <thiagohagge09@gmail.com>
2021-01-16 20:37:30 -0300

    create forms

--- quero-ser-parceira.php
+++ quero-ser-parceira.php
@@ -4,11 +4,13 @@
 
     </head>
     <body>
-        <form method="POST" action="">
+        <form method="POST" action="registro-empresa.php" enctype="multipart/form-data">
             <input type="text" name="name" id="name">
             <input type="number" name="cnpj" id="cnpj">
             <input type="text" name="description" id="description">
             <input type="file" name="logo" id="logo">
+            <input type="password" name="password" id="password">
+            <input type="submit" value="">
         </form>
     </body>
 </html>
